110818bd79a9720de1722d3720df80156ffe95cf
Jacinda Power <jjpower.writes@gmail.com>
2018-04-23 01:34:08 -0700

    added query functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -82,196 +82,75 @@ function initialPrompt() {
 
             var item = input.item_id;
             var quantity = input.stock_quantity;
+            //Query bamazon_db to confirm ID# exists in selected quantity.
+            var queryStr = 'SELECT * FROM products WHERE?';
 
-        }
-        )
-}
-//Purchase function
-//Write function that decrements units as orders are filled.
-//Need function that refills stock?
-//  var x ='stock_quantity';
-//  var y= answers.unitsSelected;
-//  var totalCost= (answers.unitsSelected * result[i].price) 
+            connection.query(queryStr, { item_id: item }, function (err, data) {
+                if (err) throw err;
+                //If user selects invalid ID#, data array will be empty.
+                console.log('ERROR: Invalid ID#.');
 
-var prodIdInfo = function () {
-    inquirer.prompt(["What is the ID# of the item you wish to purchase?"]).then
-    return result[i].item_id
+                displayInventory();
 
-
-        (answers.unitsSelected => {
-    inquirer.prompt(["For " + result[i].item_id + "there are " + x + "units available. How many do you wish to purchase?"]).then(answers => {
-
-    })
-
-});
-
-}
-if 'stock_quantity' < unitsSelected
-inquirer.prompt(["Insufficient stock! Please choose " + x + "or fewer units."]).then(answers => { })
-
-
-else inquirer.prompt([[y] + "unit(s) cost(s):$ [z]. Confirm purchase? Y or N"]).then(answers => { }))
-
-
-err
-console.log("This is not a valid command.");
-  }
-//inquirer.prompt(["There are "+ x + "units available. How many do you wish to purchase?"]).then(answers => {})
-//Needs function to crosscheck if number they select exceeds units available.
-//  If it exceeds, 
-//If answer <= to units available, calculate cost based on the number of items they selected 
-//inquirer.prompt([[y] + "units cost:$ [z]. Confirm purchase? Y or N"]).then(answers =>{})
-//Need to ask for payment method???
-//If Y, i-- 'stock_quantity' 
-//inquirer.prompt(["Thank you! Would you like to make another purchase?  Y or N"]).then(answers => {})
-//Y should loop back to id# prompt, N should log "Have a nice day. Please come again! :-)"
-
-// function which prompts the user for what action they should take
-//var itemPurchased = [];
-function start() {
-    inquirer
-        .prompt({
-            name: "want-to-purchase",
-            type: "rawlist",
-            message: "What is the ID# of the item you wish to purchase?",
-            choices: ["item_id", isNull]
-        })
-        .then(function (answer) {
-            // based on their answer, either call the bid or the post functions
-            if (answer.want - to - purchase.toUpperCase() === "item_id") {
-                displayItem();
-            }
-            else {
-                answer.want - to - purchase.toUpperCase();
-            }
-        });
-}
-
-// function to handle posting new items up for auction
-function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What is the item you would like to submit?"
-            },
-            {
-                name: "category",
-                type: "input",
-                message: "What category would you like to place your auction in?"
-            },
-            {
-                name: "startingBid",
-                type: "input",
-                message: "What would you like your starting bid to be?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query(
-                "INSERT INTO auctions SET ?",
+            } else
                 {
-                    item_name: answer.item,
-                    category: answer.category,
-                    starting_bid: answer.startingBid,
-                    highest_bid: answer.startingBid
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log("Your auction was created successfully!");
-                    // re-prompt the user for if they want to bid or post
-                    start();
-                }
-            );
-        });
-}
+                    var productData = data[0],
+                    console.log('productData = ' + JSON.stringify(productData));
+                    console.log('productData.stock_quantity= ' + productData.stock_quantity);
 
-function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
+                    //Quantity entered is in stock
+                    if(quantity < = productData.stock_quantity) {
+                        console.log('Product requested is in stock. Your total is $' + (productData.price * quantity) + '. Placing order.');
+                        //Update Inventory
+                        connection.query(updateQueryStr, function (err, data) {
+                            if (err) throw err;
+                            console.log('Your order has been placed.  Thank you!');
+                            //End Connection
+                            connection.end();
+                        })
+                    } else {
+                        console.log('Insufficient inventory.  Please modify quantity.');
+
+                        displayInventory();
+                    }
+                }
+            })
+})
+    }
+
+function displayInventory() {
+    console.log('Enter displayInventory');
+    queryStr = 'SELECT * FROM products';
+    connection.query(queryStr, function (err, data) {
         if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
+        console.log('Existing Inventory:  ');
 
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
-                }
-                else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Number of units selected exceeds our stock.  Please select fewer units.");
-                    start();
-                }
-            });
-    });
+        var strOut = '';
+        for (var i = 0; i < data.length; 1++) {
+            strOut = '';
+            strOut += 'Item ID:  ' + data[i].item_id + ' // ';
+            strOut += 'Product Name:  ' + data[i].product_name + ' // ';
+            strOut += 'Department:  ' + data[i].department_name + ' // ';
+            strOut += 'Price: $' + data[i].data.price + ' // ';
+            strOut += 'Available Quantity: ' + data[i].data.stock_quantity + ' // ';
+
+            console.log(strOut);
+
+            //Prompt quantity of item for purchase
+
+            promtUserPurchase();
+        }
+    }
+            //runBamazon will run logic
+            function runBamazon() {
+            console.log('Enter runBamazon');
+            displayInventory();
+        }
+            runBamazon();
+
 }
-  
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-});
